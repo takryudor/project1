a18ec372f091f3991edb2fc8ee45e4c9d63b67d1
Nguyễn Thành Nam <thanhnam@NAM.local>
2026-03-18 22:40:14 +0700

    func: new function added

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world!")
+# Nhận một chuỗi các số nguyên từ người dùng, cách nhau bởi dấu cách
+input_str = input("Enter a list of numbers separated by space: ")
+# Tách chuỗi thành các chuỗi con và chuyển đổi chúng thành số nguyên
+numbers = [int(num) for num in input_str.split()]
+print("The list of numbers is:"
+, numbers)

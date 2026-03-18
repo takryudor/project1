83764354938b3056b148a79ed41102112a7b7016
Duong Phan Minh Tri <mason@192.168.1.223>
2026-03-18 22:25:07 +0700

    feat: Add isPrime function

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("hello world!")
+
+def isPrime(n):
+if n <= 1:
+return False
+for i in range(2, n):
+if n % i == 0:
+return False
+return True
+print("hello, world")
+num_to_check = 7
+if isPrime(num_to_check):
+print(f"{num_to_check} is a prime number.")
+else:
+print(f"{num_to_check} is not a prime number.")
